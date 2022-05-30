78c594dbb1fdf5c37af9e8543d2e2b04b635b85b
Fotios Pechlivanis <fotispechli@gmail.com>
2022-05-30 15:57:35 +0300

    [JS] Remove paste filter.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var textarea = document.querySelector(".textarea");
-
-textarea.addEventListener("paste", function (e) {
-  e.preventDefault();
-  var text = (e.originalEvent || e).clipboardData.getData('text/plain');
-  document.execCommand("insertHTML", false, text);
-});
-
 var notes = document.querySelector('.textarea');
 var storedNotes = localStorage.getItem('notes');
 
