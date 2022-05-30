f00b603dc94463daa7f4610cfb7184173dbecfc0
Fotios Pechlivanis <fotispechli@gmail.com>
2022-05-30 16:34:17 +0300

    [JS] Fix paste as plain text.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Save on LocalStorage */
+
 var notes = document.querySelector('.textarea');
 var storedNotes = localStorage.getItem('notes');
 
@@ -7,4 +9,33 @@ notes.addEventListener('input', () => {
 
 window.addEventListener('load', () => {
   notes.textContent = storedNotes;
+});
+
+
+/* Paste As Plain Text */
+
+notes.addEventListener('paste', e => {
+  e.preventDefault();
+  const text = e.clipboardData
+    ? (e.originalEvent || e).clipboardData.getData('text/plain')
+    :
+    window.clipboardData
+      ? window.clipboardData.getData('Text')
+      : '';
+
+  if (document.queryCommandSupported('insertText')) {
+    document.execCommand('insertText', false, text);
+  } else {
+    const range = document.getSelection().getRangeAt(0);
+    range.deleteContents();
+
+    const textNode = document.createTextNode(text);
+    range.insertNode(textNode);
+    range.selectNodeContents(textNode);
+    range.collapse(false);
+
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 });
