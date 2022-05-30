b5de144b5a65079fae29be3dcc85d8c96841f606
Fotios Pechlivanis <fotispechli@gmail.com>
2022-05-30 15:20:15 +0300

    [JS] Add storing functionality.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var textarea = document.querySelector(".textarea");
 
 textarea.addEventListener("paste", function (e) {
-    e.preventDefault();
-    var text = (e.originalEvent || e).clipboardData.getData('text/plain');
-    document.execCommand("insertHTML", false, text);
+  e.preventDefault();
+  var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+  document.execCommand("insertHTML", false, text);
+});
+
+var notes = document.querySelector('.textarea');
+var storedNotes = localStorage.getItem('notes');
+
+notes.addEventListener('input', () => {
+  localStorage.setItem('notes', notes.textContent);
+});
+
+window.addEventListener('load', () => {
+  notes.textContent = storedNotes;
 });
